e38509c848cf120a368d41012f4073b623089f6d
Velas <pvelaalm@myuax.com>
2024-09-11 20:59:19 +0200

    Creación de la clase Sensor Temperatura

--- src/main/java/io/starkindustries/my_app/sensores/SensorTemperatura.java
+++ src/main/java/io/starkindustries/my_app/sensores/SensorTemperatura.java
@@ -1,4 +1,28 @@
 package io.starkindustries.my_app.sensores;
 
-public class SensorTemperatura {
+public class SensorTemperatura extends Sensor {
+    private int temperaturaActual;
+
+    public SensorTemperatura(String idSensor, String ubicacion, boolean estado, int sensibilidad, int temperaturaActual) {
+        super(idSensor, TipoSensor.Tipo.TEMPERATURA, ubicacion, estado, sensibilidad);
+        this.temperaturaActual = temperaturaActual;
+    }
+
+    @Override
+    public void activar() {
+        System.out.println("Sensor de temperatura activado.");
+    }
+
+    @Override
+    public void desactivar() {
+        System.out.println("Sensor de temperatura desactivado.");
+    }
+
+    public int getTemperaturaActual() {
+        return temperaturaActual;
+    }
+
+    public void setTemperaturaActual(int temperaturaActual) {
+        this.temperaturaActual = temperaturaActual;
+    }
 }
